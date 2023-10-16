4ad854b15b42588505cc8f4e76f1578240e6818c
LukeShinoda <luc.tph@gmail.com>
2023-10-16 16:30:51 +0200

    cleanup, bugfixes and finally a version that fits with lj/cut again. However all flags were removed!

--- src/EXTRA-PAIR/pair_lj_eopp.cpp
+++ src/EXTRA-PAIR/pair_lj_eopp.cpp
@@ -89,6 +89,9 @@ void PairLJEopp::compute(int eflag, int vflag)
   double *special_lj = force->special_lj;
   int newton_pair = force->newton_pair;
 
+
+  double force1, force2, force3;
+
   inum = list->inum;
   ilist = list->ilist;
   numneigh = list->numneigh;
@@ -114,31 +117,23 @@ void PairLJEopp::compute(int eflag, int vflag)
       dely = ytmp - x[j][1];
       delz = ztmp - x[j][2];
       rsq = delx * delx + dely * dely + delz * delz;
+      r = sqrt(rsq);
       jtype = type[j];
       if (rsq < cutsq[itype][jtype]) {
-        /**force calculation*/
-          if (debug){
-          r2inv = 1.0 / rsq;
-          r6inv = r2inv * r2inv * r2inv;
-          forcelj = r6inv * (48.0 * epsilon[itype][jtype] * pow(sigma[itype][jtype], 12.0) * r6inv - 24.0 * epsilon[itype][jtype] * pow(sigma[itype][jtype], 6.0));
-          fpair = factor_lj * forcelj * r2inv;
-          oldfpair = fpair;
-          }
-          //new force
-          r = sqrt(rsq);
-          forcelj = lj1[itype][jtype] / pow(r,n1[itype][jtype]);
+          /* we will treat 1/r components if no derivate would be formed, multiply with r2inv after calculation - just like in the original*/
+          force1 =  lj1[itype][jtype] * (1.0 / pow(r, n1[itype][jtype] + 1.0));
           //2nd term, chain rule first part
-          //printf("the force is %f, we will add %f and then add %f\n", forcelj,lj2[itype][jtype] / pow(r,n2[itype][jtype]) * cos(k[itype][jtype]*r + p[itype][jtype]),lj3[itype][jtype] * k[itype][jtype] / pow(r,n2[itype][jtype]+1.0)*sin(k[itype][jtype]*r + p[itype][jtype]) );
-          forcelj +=  lj2[itype][jtype] / pow(r,n2[itype][jtype]) * cos(k[itype][jtype]*r + p[itype][jtype]);
+          force2 = lj2[itype][jtype] * (1.0 / pow(r,n2[itype][jtype] + 1.0)) * cos(k[itype][jtype]*r + p[itype][jtype]);
           //2nd term,chain rul second part
-          forcelj +=  lj3[itype][jtype] * k[itype][jtype] / pow(r,n2[itype][jtype]+1.0)*sin(k[itype][jtype]*r + p[itype][jtype]);
-          fpair = forcelj * (1.0/rsq);
+          force3 = (lj4[itype][jtype] * k[itype][jtype]) * (1.0 / pow(r,n2[itype][jtype])) * sin(k[itype][jtype]*r + p[itype][jtype]);
+          
+          forcelj = force1 + force2 + force3;
+
+          fpair = forcelj * (1.0 / r) * factor_lj;
           if (debug && abs(fpair-oldfpair)>0.0001){
             printf("new fpair is %f , old one is %f, difference is %f \n", fpair, oldfpair, fpair-oldfpair );
           }
-        //the original implementation is calculating a factor of r^(-12) and r^(-6) for the force
-        // multiplied by r^-2 for fpair
-        //here the corresponding distances are multiplied back again which makes us reach a factor of r^-13 and r^-7 - as wanted
+        //seems like those shall be forces!
         f[i][0] += delx * fpair;
         f[i][1] += dely * fpair;
         f[i][2] += delz * fpair;
@@ -157,12 +152,12 @@ void PairLJEopp::compute(int eflag, int vflag)
               oldevdwl = evdwl;
             }
             //first term
-            evdwl = lj3[itype][jtype] / pow(r,n1[itype][jtype]);
+            evdwl = lj3[itype][jtype] * (1.0 / pow(r,n1[itype][jtype]));
             //second term
-            evdwl += lj4[itype][jtype] / pow(r,n2[itype][jtype]) * cos(k[itype][jtype]*r + p[itype][jtype]);
-            //printf("evdwl is sadly %f. I will even add %f \n", evdwl, offset[itype][jtype]);
+            evdwl += lj4[itype][jtype] * (1.0/ pow(r,n2[itype][jtype])) * cos(k[itype][jtype]*r + p[itype][jtype]);
+            //printf("evdwl is %f. I will even add %f \n", evdwl, offset[itype][jtype]);
             evdwl-= offset[itype][jtype];
-        
+            evdwl*=factor_lj;
             if (debug && abs(evdwl-oldevdwl) > 0.0001) {
               printf("evdwl old is %f, new one is %f \n", oldevdwl, evdwl);
             }
@@ -179,257 +174,20 @@ void PairLJEopp::compute(int eflag, int vflag)
 
 void PairLJEopp::compute_inner()
 {
-  printf("compute inner");
-  int i, j, ii, jj, inum, jnum, itype, jtype;
-  double xtmp, ytmp, ztmp, delx, dely, delz, fpair;
-  double rsq, r2inv, r6inv, forcelj, factor_lj, rsw;
-  int *ilist, *jlist, *numneigh, **firstneigh;
 
-  double **x = atom->x;
-  double **f = atom->f;
-  int *type = atom->type;
-  int nlocal = atom->nlocal;
-  double *special_lj = force->special_lj;
-  int newton_pair = force->newton_pair;
-
-  inum = list->inum_inner;
-  ilist = list->ilist_inner;
-  numneigh = list->numneigh_inner;
-  firstneigh = list->firstneigh_inner;
-
-  double cut_out_on = cut_respa[0];
-  double cut_out_off = cut_respa[1];
-
-  double cut_out_diff = cut_out_off - cut_out_on;
-  double cut_out_on_sq = cut_out_on * cut_out_on;
-  double cut_out_off_sq = cut_out_off * cut_out_off;
-
-  // loop over neighbors of my atoms
-
-  for (ii = 0; ii < inum; ii++) {
-    i = ilist[ii];
-    xtmp = x[i][0];
-    ytmp = x[i][1];
-    ztmp = x[i][2];
-    itype = type[i];
-    jlist = firstneigh[i];
-    jnum = numneigh[i];
-
-    for (jj = 0; jj < jnum; jj++) {
-      j = jlist[jj];
-      factor_lj = special_lj[sbmask(j)];
-      j &= NEIGHMASK;
-
-      delx = xtmp - x[j][0];
-      dely = ytmp - x[j][1];
-      delz = ztmp - x[j][2];
-      rsq = delx * delx + dely * dely + delz * delz;
-
-      if (rsq < cut_out_off_sq) {
-        r2inv = 1.0 / rsq;
-        r6inv = r2inv * r2inv * r2inv;
-        jtype = type[j];
-        forcelj = r6inv * (lj1[itype][jtype] * r6inv - lj2[itype][jtype]);
-        
-
-        fpair = factor_lj * forcelj * r2inv;
-        if (rsq > cut_out_on_sq) {
-          rsw = (sqrt(rsq) - cut_out_on) / cut_out_diff;
-          fpair *= 1.0 - rsw * rsw * (3.0 - 2.0 * rsw);
-        }
-
-        f[i][0] += delx * fpair;
-        f[i][1] += dely * fpair;
-        f[i][2] += delz * fpair;
-        if (newton_pair || j < nlocal) {
-          f[j][0] -= delx * fpair;
-          f[j][1] -= dely * fpair;
-          f[j][2] -= delz * fpair;
-        }
-      }
-    }
-  }
 }
 
 /* ---------------------------------------------------------------------- */
 
 void PairLJEopp::compute_middle()
 {
-  printf("compute middle");
-  int i, j, ii, jj, inum, jnum, itype, jtype;
-  double xtmp, ytmp, ztmp, delx, dely, delz, fpair;
-  double rsq, r2inv, r6inv, forcelj, factor_lj, rsw;
-  int *ilist, *jlist, *numneigh, **firstneigh;
 
-  double **x = atom->x;
-  double **f = atom->f;
-  int *type = atom->type;
-  int nlocal = atom->nlocal;
-  double *special_lj = force->special_lj;
-  int newton_pair = force->newton_pair;
-
-  inum = list->inum_middle;
-  ilist = list->ilist_middle;
-  numneigh = list->numneigh_middle;
-  firstneigh = list->firstneigh_middle;
-
-  double cut_in_off = cut_respa[0];
-  double cut_in_on = cut_respa[1];
-  double cut_out_on = cut_respa[2];
-  double cut_out_off = cut_respa[3];
-
-  double cut_in_diff = cut_in_on - cut_in_off;
-  double cut_out_diff = cut_out_off - cut_out_on;
-  double cut_in_off_sq = cut_in_off * cut_in_off;
-  double cut_in_on_sq = cut_in_on * cut_in_on;
-  double cut_out_on_sq = cut_out_on * cut_out_on;
-  double cut_out_off_sq = cut_out_off * cut_out_off;
-
-  // loop over neighbors of my atoms
-
-  for (ii = 0; ii < inum; ii++) {
-    i = ilist[ii];
-    xtmp = x[i][0];
-    ytmp = x[i][1];
-    ztmp = x[i][2];
-    itype = type[i];
-    jlist = firstneigh[i];
-    jnum = numneigh[i];
-
-    for (jj = 0; jj < jnum; jj++) {
-      j = jlist[jj];
-      factor_lj = special_lj[sbmask(j)];
-      j &= NEIGHMASK;
-
-      delx = xtmp - x[j][0];
-      dely = ytmp - x[j][1];
-      delz = ztmp - x[j][2];
-      rsq = delx * delx + dely * dely + delz * delz;
-
-      if (rsq < cut_out_off_sq && rsq > cut_in_off_sq) {
-        r2inv = 1.0 / rsq;
-        r6inv = r2inv * r2inv * r2inv;
-        jtype = type[j];
-        forcelj = r6inv * (lj1[itype][jtype] * r6inv - lj2[itype][jtype]);
-        fpair = factor_lj * forcelj * r2inv;
-        if (rsq < cut_in_on_sq) {
-          rsw = (sqrt(rsq) - cut_in_off) / cut_in_diff;
-          fpair *= rsw * rsw * (3.0 - 2.0 * rsw);
-        }
-        if (rsq > cut_out_on_sq) {
-          rsw = (sqrt(rsq) - cut_out_on) / cut_out_diff;
-          fpair *= 1.0 + rsw * rsw * (2.0 * rsw - 3.0);
-        }
-
-        f[i][0] += delx * fpair;
-        f[i][1] += dely * fpair;
-        f[i][2] += delz * fpair;
-        if (newton_pair || j < nlocal) {
-          f[j][0] -= delx * fpair;
-          f[j][1] -= dely * fpair;
-          f[j][2] -= delz * fpair;
-        }
-      }
-    }
-  }
 }
 
 /* ---------------------------------------------------------------------- */
 
 void PairLJEopp::compute_outer(int eflag, int vflag)
 {
-  printf("compute outer");
-  int i, j, ii, jj, inum, jnum, itype, jtype;
-  double xtmp, ytmp, ztmp, delx, dely, delz, evdwl, fpair;
-  double rsq, r2inv, r6inv, forcelj, factor_lj, rsw;
-  int *ilist, *jlist, *numneigh, **firstneigh;
-
-  evdwl = 0.0;
-  ev_init(eflag, vflag);
-
-  double **x = atom->x;
-  double **f = atom->f;
-  int *type = atom->type;
-  int nlocal = atom->nlocal;
-  double *special_lj = force->special_lj;
-  int newton_pair = force->newton_pair;
-
-  inum = list->inum;
-  ilist = list->ilist;
-  numneigh = list->numneigh;
-  firstneigh = list->firstneigh;
-
-  double cut_in_off = cut_respa[2];
-  double cut_in_on = cut_respa[3];
-
-  double cut_in_diff = cut_in_on - cut_in_off;
-  double cut_in_off_sq = cut_in_off * cut_in_off;
-  double cut_in_on_sq = cut_in_on * cut_in_on;
-  // loop over neighbors of my atoms
-
-  for (ii = 0; ii < inum; ii++) {
-    i = ilist[ii];
-    xtmp = x[i][0];
-    ytmp = x[i][1];
-    ztmp = x[i][2];
-    itype = type[i];
-    jlist = firstneigh[i];
-    jnum = numneigh[i];
-
-    for (jj = 0; jj < jnum; jj++) {
-      j = jlist[jj];
-      factor_lj = special_lj[sbmask(j)];
-      j &= NEIGHMASK;
-
-      delx = xtmp - x[j][0];
-      dely = ytmp - x[j][1];
-      delz = ztmp - x[j][2];
-      rsq = delx * delx + dely * dely + delz * delz;
-      jtype = type[j];
-
-      if (rsq < cutsq[itype][jtype]) {
-        if (rsq > cut_in_off_sq) {
-          r2inv = 1.0 / rsq;
-          r6inv = r2inv * r2inv * r2inv;
-          forcelj = r6inv * (lj1[itype][jtype] * r6inv - lj2[itype][jtype]);
-
-          fpair = factor_lj * forcelj * r2inv;
-          if (rsq < cut_in_on_sq) {
-            rsw = (sqrt(rsq) - cut_in_off) / cut_in_diff;
-            fpair *= rsw * rsw * (3.0 - 2.0 * rsw);
-          }
-
-          f[i][0] += delx * fpair;
-          f[i][1] += dely * fpair;
-          f[i][2] += delz * fpair;
-          if (newton_pair || j < nlocal) {
-            f[j][0] -= delx * fpair;
-            f[j][1] -= dely * fpair;
-            f[j][2] -= delz * fpair;
-          }
-        }
-        //energy needed only
-        if (eflag) {
-          r2inv = 1.0 / rsq;
-          r6inv = r2inv * r2inv * r2inv;
-          evdwl = r6inv * (lj3[itype][jtype] * r6inv - lj4[itype][jtype]) - offset[itype][jtype];
-          evdwl *= factor_lj;
-        }
-        //virial needed
-        if (vflag) {
-          if (rsq <= cut_in_off_sq) {
-            r2inv = 1.0 / rsq;
-            r6inv = r2inv * r2inv * r2inv;
-            forcelj = r6inv * (lj1[itype][jtype] * r6inv - lj2[itype][jtype]);
-            fpair = factor_lj * forcelj * r2inv;
-          } else if (rsq < cut_in_on_sq)
-            fpair = factor_lj * forcelj * r2inv;
-        }
-        //either one or the other one
-        if (evflag) ev_tally(i, j, nlocal, newton_pair, evdwl, 0.0, fpair, delx, dely, delz);
-      }
-    }
-  }
 }
 
 /* ----------------------------------------------------------------------
@@ -587,11 +345,11 @@ double PairLJEopp::init_one(int i, int j)
 
   //constants from derivative of first term in potential
   // d/dr (c1/r^n1)
-  lj1[i][j] =   n1[i][j] * c1[i][j];
+  lj1[i][j] = n1[i][j] * c1[i][j];
   printf("old lj1 is %f, new lj1 is %f \n", 48.0 * epsilon[i][j] * pow(sigma[i][j], 12.0), lj1[i][j] );
   //outter derivative of second term in potential
   //d/dr (c2/r^n2*cos(kr+phi)) --> chain rule lj2 first term, lj3 second term (constants not touched)
-  lj2[i][j] =   n2[i][j] * c2[i][j];
+  lj2[i][j] = n2[i][j] * c2[i][j];
   printf("old lj2 is %f, new lj2 is %f \n", 24.0 * epsilon[i][j] * pow(sigma[i][j], 6.0), lj2[i][j] );
   //no derivative, first term
   lj3[i][j] = c1[i][j];
@@ -604,7 +362,7 @@ double PairLJEopp::init_one(int i, int j)
   We have V(r) =c1/r^n1 + C2/r^n2*cos(kr+p)
   **/
 
-//TODO: consider new offset
+//for shift command
   if (offset_flag && (cut[i][j] > 0.0)) {
     /*double ratio = sigma[i][j] / cut[i][j]; 
     offset[i][j] = 4.0 * epsilon[i][j] * (pow(ratio, 12.0) - pow(ratio, 6.0));
@@ -613,6 +371,7 @@ double PairLJEopp::init_one(int i, int j)
     double ratio2 = c2[i][j]/pow(cut[i][j],n2[i][j]);
     printf("ratios are %f,%f\n", ratio1, ratio2);
     offset[i][j] = ratio1 + ratio2;
+    offset[i][j] = 0.0;
     printf("offset for %d,%d is %f\n",i,j , offset[i][j]);
   } else
     offset[i][j] = 0.0;
@@ -622,36 +381,6 @@ double PairLJEopp::init_one(int i, int j)
   lj3[j][i] = lj3[i][j];
   lj4[j][i] = lj4[i][j];
   offset[j][i] = offset[i][j];
-
-  // check interior rRESPA cutoff
-
-  if (cut_respa && cut[i][j] < cut_respa[3])
-    error->all(FLERR, "Pair cutoff < Respa interior cutoff");
-
-  // compute I,J contribution to long-range tail correction
-  // count total # of atoms of type I and J via Allreduce
-
-  if (tail_flag) {
-    int *type = atom->type;
-    int nlocal = atom->nlocal;
-
-    double count[2], all[2];
-    count[0] = count[1] = 0.0;
-    for (int k = 0; k < nlocal; k++) {
-      if (type[k] == i) count[0] += 1.0;
-      if (type[k] == j) count[1] += 1.0;
-    }
-    MPI_Allreduce(count, all, 2, MPI_DOUBLE, MPI_SUM, world);
-
-    double sig2 = sigma[i][j] * sigma[i][j];
-    double sig6 = sig2 * sig2 * sig2;
-    double rc3 = cut[i][j] * cut[i][j] * cut[i][j];
-    double rc6 = rc3 * rc3;
-    double rc9 = rc3 * rc6;
-    double prefactor = 8.0 * MY_PI * all[0] * all[1] * epsilon[i][j] * sig6 / (9.0 * rc9);
-    etail_ij = prefactor * (sig6 - 3.0 * rc6);
-    ptail_ij = 2.0 * prefactor * (2.0 * sig6 - 3.0 * rc6);
-  }
 
   return cut[i][j];
 }
